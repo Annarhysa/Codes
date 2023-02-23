599d08e411d6089dfc7bb8503c0ca519869bd0a0
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:41:45 +0530

    Create Size-of-data-types.c

---
+++ C Programming/Size-of-data-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char c;
+    float f; 
+    double d;
+    printf("Size of int = %ld\n", sizeof(i));
+    printf("Size of char = %ld\n", sizeof(c));
+    printf("Size of float = %ld\n", sizeof(f));
+    printf("Size of double = %ld\n", sizeof(d));
+    return 0;
+}
+
