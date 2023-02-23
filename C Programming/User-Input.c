0e40305160aa2576f39ad7ac399f0e5aab8bf258
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:39:45 +0530

    Create User-Input.c

---
+++ C Programming/User-Input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int no1, no2;
+    printf("Enter Two Digits ");
+    scanf("%d %d", &no1, &no2);
+    printf("Sum of The Given Digits = %d", no1+no2);
+    return 0;
+}
