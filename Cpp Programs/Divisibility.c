87d7ce5a1e4a3c74f854572ede4addf9597e0c13
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:23:17 +0530

    Create Divisibility.c

---
+++ Cpp Programs/Divisibility.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int i, j;
+  for (i = 1000; i <= 2000; i++)
+	    {if (i % 8 == 0 && i % 5 == 0)
+	    cout<<i<<endl;}
+
+  return 0;
+}
