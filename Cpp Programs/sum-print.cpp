473dd69bff912aec96c4d2b856b899d6d383944a
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:26:43 +0530

    Create sum-print.cpp

---
+++ Cpp Programs/sum-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int num1, num2, sum =0;
+cout<<"Enter two numbers: ";
+cin>>num1>>num2;
+sum = num1+num2;
+if(sum >= 105 && sum <=200)
+cout<<"200";
+else
+cout<<sum;
+
+    return 0;
+}
