7aa0b336521f28691749d07597325d5a19645524
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:20:12 +0530

    Create Sum-of-two.cpp

---
+++ Cpp Programs/Sum-of-two.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+    cout<<"Enter 2 numbers separated by a space: "<<endl;
+    cin>>num1>>num2;
+    sum = num1 + num2;
+    cout<<num1<<"+"<<num2<<"="<<sum<<endl;
+    return 0;
+}
