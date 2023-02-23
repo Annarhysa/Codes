f0d5d091c85007c02efb7747c07f57bcdf2b1fd2
Annarhysa Albert <annarhysa13@gmail.com>
2023-02-23 20:39:24 +0530

    Create Operations.c

---
+++ C Programming/Operations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a=10, b=20, c, d, e, f;
+    c= a+b;
+    printf("a+b = %d\n", c);
+    d= b-a;
+    printf("b-a = %d\n", d);
+    e= a*b;
+    printf("a*b = %d\n", e);
+    f= b/a;
+    printf("b/a = %d\n", f);
+    
+
+     return 0;
+}
